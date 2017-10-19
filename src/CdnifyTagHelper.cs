8b182516f8d0598095303a63dcbae3a17c5f9da6
Mads Kristensen <post@madskristensen.net>
2017-10-19 14:53:19 -0700

    Fixed issue with persistent attribute [release]

--- src/CdnifyTagHelper.cs
+++ src/CdnifyTagHelper.cs
@@ -4,10 +4,11 @@ using Microsoft.Extensions.Configuration;
 
 namespace WebEssentials.AspNetCore.CdnTagHelpers
 {
-    [HtmlTargetElement("*", Attributes = "cdnify")]
+    [HtmlTargetElement("*", Attributes = _attrName)]
     public class CdnifyTagHelper : TagHelper
     {
         private string _cdnUrl;
+        private const string _attrName = "cdnify";
 
         public CdnifyTagHelper(IConfiguration config)
         {
@@ -16,15 +17,16 @@ namespace WebEssentials.AspNetCore.CdnTagHelpers
 
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
+            output?.Attributes?.RemoveAll(_attrName);
+
             if (string.IsNullOrWhiteSpace(_cdnUrl) || string.IsNullOrEmpty(output.TagName))
             {
                 return;
             }
 
-            string html = output.Content.IsModified ? output.Content.GetContent() :(await output.GetChildContentAsync()).GetContent();
+            string html = output.Content.IsModified ? output.Content.GetContent() : (await output.GetChildContentAsync()).GetContent();
             string cdnified = html.CdnifyHtmlImageUrls(_cdnUrl);
 
-            output.Attributes.RemoveAll("cdnify");
             output.Content.SetHtmlContent(cdnified);
         }
     }
